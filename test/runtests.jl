3bd50d19130e7015ba3f9b9b9640770a7055c2c7
WDWidanage <w.d.widanage@outlook.com>
2025-06-22 18:50:55 +0100

    Updated package to read json to DataFrames

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
 using BatteryDynamics
 using Test
 
-@testset "BatteryDynamics.jl" begin
-    # Write your tests here.
-    @test BatteryDynamics.multiply() == 12
-    @test BatteryDynamics.multiply(2) == 8
-    @test BatteryDynamics.multiply(2, 3) == 6
+@testset "BatteryDynamics.jl" begin # test the BatteryDynamics.jl package
+
+
+    df, meta_data = read_ocv_data("/Users/wdwidanage/Library/CloudStorage/OneDrive-Personal/Data/Battery_Data/OCV_Database/OCV_20Si50Gr.json")
+    @test round(df.state_of_lithiation[1], digits=3) == 0.002
+    @test meta_data.active_material[1] == "20Si50Gr"
+
+
 end
