e6f44157d443be04953a9977f7bc1fa51cdacc03
WDWidanage <w.d.widanage@outlook.com>
2025-09-04 22:18:25 +0100

    updated testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,15 @@ using Test
 
 @testset "BatteryDynamics.jl" begin # test the BatteryDynamics.jl package
 
+    filename = "test/OCV_20Si50Gr.json"
+    if isfile(filename)
+        df, meta_data = read_ocv_data("test/OCV_20Si50Gr.json")
+        @test round(df.state_of_lithiation[1], digits=3) == 0.002
+        @test meta_data.active_material[1] == "20Si50Gr"    
+    else
+        @warn "Test file $filename not found; skipping related tests."
+    end
 
-    df, meta_data = read_ocv_data("test/OCV_20Si50Gr.json")
-    @test round(df.state_of_lithiation[1], digits=3) == 0.002
-    @test meta_data.active_material[1] == "20Si50Gr"
 
 
 end
