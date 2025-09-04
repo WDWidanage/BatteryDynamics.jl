be5e8665c8f4c619923bdbf4c925d89948cefae4
WDWidanage <w.d.widanage@outlook.com>
2025-09-04 22:01:27 +0100

    added an ocv json file for testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 @testset "BatteryDynamics.jl" begin # test the BatteryDynamics.jl package
 
 
-    df, meta_data = read_ocv_data("/Users/wdwidanage/Library/CloudStorage/OneDrive-Personal/Data/Battery_Data/OCV_Database/OCV_20Si50Gr.json")
+    df, meta_data = read_ocv_data("test/OCV_20Si50Gr.json")
     @test round(df.state_of_lithiation[1], digits=3) == 0.002
     @test meta_data.active_material[1] == "20Si50Gr"
 
